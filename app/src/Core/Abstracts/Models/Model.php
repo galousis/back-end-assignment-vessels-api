ec498f00eb2dac2c446b1e6e31bb3d93a96ee366
Thanos Theodorakopoulos <galousis@gmail.com>
2020-05-12 19:20:52 +0300

    Take care of Migrations

---
+++ app/src/Core/Abstracts/Models/Model.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace MarineTraffic\Core\Abstracts\Models;
+
+use Illuminate\Database\Eloquent\Model as LaravelEloquentModel;
+
+abstract class Model extends LaravelEloquentModel
+{
+
+}
